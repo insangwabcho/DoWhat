0fc12feeec5a2c6a09fe1ba0248402cfa65b4728
insangwabcho <dhkq92@icloud.com>
2017-06-15 16:43:07 +0900

    gdgd

--- app/src/main/java/com/comnawa/dowhat/insang/Preferences.java
+++ app/src/main/java/com/comnawa/dowhat/insang/Preferences.java
@@ -7,5 +7,7 @@ public class Preferences extends android.preference.PreferenceActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+//    skkk
   }
 }
