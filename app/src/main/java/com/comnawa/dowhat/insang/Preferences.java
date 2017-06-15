91264c852cf5bbd2eef063798a4e46a49129fd74
insangwabcho <dhkq92@icloud.com>
2017-06-15 16:55:40 +0900

    gdgd

--- app/src/main/java/com/comnawa/dowhat/insang/Preferences.java
+++ app/src/main/java/com/comnawa/dowhat/insang/Preferences.java
@@ -1,6 +1,10 @@
 package com.comnawa.dowhat.insang;
 
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+import com.comnawa.dowhat.R;
 
 public class Preferences extends android.preference.PreferenceActivity {
 
@@ -8,6 +12,15 @@ public class Preferences extends android.preference.PreferenceActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-//    skkk
+    getFragmentManager().beginTransaction().replace(android.R.id.content, new MyFragment()).commit();
+
+  }
+
+  public static class MyFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      addPreferencesFromResource(R.xml.preference_insang);
+    }
   }
 }
