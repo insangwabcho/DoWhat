6f8ea685884fb91065c8b756cf156ad9d542c2a3
insangwabcho <dhkq92@icloud.com>
2017-06-16 20:27:44 +0900

    insang

--- app/src/main/java/com/comnawa/dowhat/LoadActivity.java
+++ app/src/main/java/com/comnawa/dowhat/LoadActivity.java
@@ -1,31 +1,53 @@
 package com.comnawa.dowhat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 public class LoadActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    setTheme(android.R.style.Theme_NoTitleBar_Fullscreen);
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     setContentView(R.layout.load_insang);
     super.onCreate(savedInstanceState);
-    ImageView img= (ImageView)findViewById(R.id.imageView2);
 
-    try {
-      Thread.sleep(1000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
+    Loading loading = new Loading();
+    loading.execute();
+
+  }
+
+  private class Loading extends AsyncTask<Void, Void, Void> {
+
+    ProgressDialog dialog = new ProgressDialog(LoadActivity.this);
+
+    @Override
+    protected void onPreExecute() {
+      dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+      dialog.setMessage("로딩중입니다. 잠시만기다려주세요");
+      dialog.show();
+      super.onPreExecute();
     }
 
-    Toast.makeText(this, "환영합니다! 오늘뭐할까?!", Toast.LENGTH_SHORT).show();
+    @Override
+    protected Void doInBackground(Void... params) {
+      try {
+        Thread.sleep(1500);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      return null;
+    }
 
-    Intent intent= new Intent(this, MainActivity.class);
-    startActivity(intent);
+    @Override
+    protected void onPostExecute(Void aVoid) {
+      super.onPostExecute(aVoid);
+      Intent intent = new Intent(LoadActivity.this, MainActivity.class);
+      startActivity(intent);
+      finish();
+    }
   }
 }
