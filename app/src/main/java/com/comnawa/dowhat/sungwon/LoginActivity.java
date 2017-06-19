87fdf80d1f8c057b8c8a31867bc1f83cc905a97b
sungwon <leetjddnjs12@naver.com>
2017-06-19 15:51:23 +0900

    asdf

--- app/src/main/java/com/comnawa/dowhat/sungwon/LoginActivity.java
+++ app/src/main/java/com/comnawa/dowhat/sungwon/LoginActivity.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.comnawa.dowhat.R;
+import com.comnawa.dowhat.insang.PrefManager;
 import com.comnawa.dowhat.sangjin.CalendarActivity;
 
 import org.apache.http.HttpResponse;
@@ -25,12 +26,12 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
     EditText editid, editpwd;
     Button btnLogin, btnSignUp;
     CheckBox cb;
-    String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +41,17 @@ public class LoginActivity extends AppCompatActivity {
         editpwd = (EditText) findViewById(R.id.editpwd);
         btnSignUp = (Button) findViewById(R.id.btnSignUp);
         btnLogin = (Button) findViewById(R.id.btnLogin);
+        cb = (CheckBox)findViewById(R.id.cb);
+        if(new PrefManager(this).getAutoLogin()){
+            Intent intent = new Intent(LoginActivity.this,CalendarActivity.class);
+            HashMap<String,String> map = new PrefManager(this).getUserInfo();
+            intent.putExtra("id",map.get("id"));
+            intent.putExtra("password",map.get("password"));
+            intent.putExtra("name",map.get("name"));
+            intent.putExtra("friendid",map.get("friendid"));
+            startActivity(intent);
+            finish();
+        }
         //회원가입 버튼 클릭 이벤트
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,12 +88,17 @@ public class LoginActivity extends AppCompatActivity {
                                 String pwd = jlist.get("password").toString();
                                 String name = jlist.get("name").toString();
                                 String friendid = jlist.get("friendid").toString();
+                                PrefManager pm = new PrefManager(LoginActivity.this);
+                                if(cb.isChecked()){
+                                    pm.setAutoLogin(id,pwd,name,friendid);
+                                }
                                 Intent intent = new Intent(LoginActivity.this, CalendarActivity.class);
                                 intent.putExtra("id", id);
                                 intent.putExtra("password", pwd);
                                 intent.putExtra("name", name);
                                 intent.putExtra("friendid", friendid);
                                 startActivity(intent);
+                                finish();
                             } else {
                                 runOnUiThread(new Runnable() {
                                     @Override
