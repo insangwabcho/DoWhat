3c50617f49127731cc883eba46d835c10b2a7fff
Diaspora <rtkwl0714@naver.com>
2017-06-20 17:51:50 +0900

    kk

--- app/src/main/java/com/comnawa/dowhat/kwanwoo/CalendarCoreActivity.java
+++ app/src/main/java/com/comnawa/dowhat/kwanwoo/CalendarCoreActivity.java
@@ -9,7 +9,6 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.comnawa.dowhat.R;
-import com.google.android.gms.maps.MapFragment;
 
 public class CalendarCoreActivity extends AppCompatActivity {
   // 변수선언
@@ -20,34 +19,39 @@ public class CalendarCoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_core_kwanwoo);
-    //생성
-        txtTitle = (EditText)findViewById(R.id.txtTitle);
-        txtPlace = (EditText)findViewById(R.id.txtPlace);
-        txtStart = (EditText)findViewById(R.id.txtStart);
-        txtEnd = (EditText)findViewById(R.id.txtEnd);
-        btnColor = (ImageButton)findViewById(R.id.btnColor);
-        btnPlace = (ImageButton)findViewById(R.id.btnPlace);
-    //버튼클릭 이벤트
-      btnColor.setOnClickListener(new View.OnClickListener() {
+        //생성
+        txtTitle = (EditText) findViewById(R.id.txtTitle);
+        txtPlace = (EditText) findViewById(R.id.txtPlace);
+        txtStart = (EditText) findViewById(R.id.txtStart);
+        txtEnd = (EditText) findViewById(R.id.txtEnd);
+        btnColor = (ImageButton) findViewById(R.id.btnColor);
+        btnPlace = (ImageButton) findViewById(R.id.btnPlace);
+
+        //버튼클릭 이벤트
+        btnColor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Dialog cb = new Dialog(CalendarCoreActivity.this);
                 cb.setTitle("Color버튼 선택");
                 TextView text = new TextView(CalendarCoreActivity.this);
                 text.setText("Color버튼의 내용");
-              //  cb.setContentView(text); //color버튼의 내용
-         //다이얼로그의 화면을 XML로 지정
+                //  cb.setContentView(text); //color버튼의 내용
+                //다이얼로그의 화면을 XML로 지정
                 cb.setContentView(R.layout.color_choice_kwanwoo);
                 cb.show();  //컬러버튼을 화면에 표시
             }
         });
-      btnPlace.setOnClickListener(new View.OnClickListener() {
-        MapFragment fragment;
-          @Override
-          public void onClick(View v) {
-              MapFragment pc = new MapFragment();
+        btnPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              Dialog poch = new Dialog(CalendarCoreActivity.this);
+                poch.setContentView(R.layout.position_kwanwoo);
+                poch.show();
 
-          }
-      });
+
+
+            }
+        });
+
     }// onCreate
 }
--- app/src/main/java/com/comnawa/dowhat/kwanwoo/PositionActivity.java
+++ app/src/main/java/com/comnawa/dowhat/kwanwoo/PositionActivity.java
@@ -1,15 +1,71 @@
 package com.comnawa.dowhat.kwanwoo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.comnawa.dowhat.R;
+import com.comnawa.dowhat.insang.DoWhat;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class PositionActivity extends AppCompatActivity {
+import java.util.List;
 
+public class PositionActivity extends AppCompatActivity implements OnMapReadyCallback {
+    EditText editPlace;
+    Button btnSearch;
+    MapFragment fragment;
+    private GoogleMap map;
+
+    //GoogleMap
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.position_kwanwoo);
+
+        editPlace = (EditText) findViewById(R.id.fragment1);
+        fragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fragment1);
+        //미동기적 방식으로 맵 로딩
+        fragment.getMapAsync(this);
+    }
+
+    //로딩완료시 지도 호출
+    @Override
+    public void onMapReady(GoogleMap Map) {
+        this.map = map;
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        DoWhat.checkPermission(this, DoWhat.access_fine_location, DoWhat.access_coarse_location);
+        map.setMyLocationEnabled(true);
+
+        map.getUiSettings().setZoomControlsEnabled(true);
+    }
+    public void search(View v){
+        String place = editPlace.getText().toString();
+        Geocoder corder = new Geocoder(this);
+        List<Address> list = null;
+        try {
+            list = corder.getFromLocationName(place, 1);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        Address addr = list.get(0);
+        double lat = addr.getLatitude();    //위도
+        double log = addr.getLongitude();   //경도
+
+        LatLng geoPoint = new LatLng(lat, log);
+        map.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(geoPoint,15));
+        MarkerOptions marker = new MarkerOptions();
+        marker.position(geoPoint);
+        marker.title(editPlace.getText().toString());
+        map.addMarker(marker);
     }
 }
