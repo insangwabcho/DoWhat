83628a7a371c9ff85d8444846ace74231b9d01b9
sangjin <sangjin0309@naver.com>
2017-07-03 15:18:35 +0900

    hi

--- app/src/main/java/com/comnawa/dowhat/sangjin/CalendarActivity.java
+++ app/src/main/java/com/comnawa/dowhat/sangjin/CalendarActivity.java
@@ -203,8 +203,8 @@ public class CalendarActivity extends ListActivity implements Serializable {
     public boolean onOptionsItemSelected(MenuItem item) {
         //친구찾기 버튼을 눌렀을때 처리
         if (item.getItemId() == R.id.addFriend) {
-//            Intent intent=new Intent(this, AddFriendActivity.class);
-//            startActivity(intent);
+            Intent intent=new Intent(this, AddFriendActivity.class);
+            startActivity(intent);
             //환경설정 버튼을 눌렀을때 처리
         } else if (item.getItemId() == R.id.action_settings) {
             startActivity(new Intent(CalendarActivity.this, Preferences.class));
